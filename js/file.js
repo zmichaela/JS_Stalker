a80eabee9b59c4bcc7525cd396aa6b60922e0de2
zmichaela <44002738+zmichaela@users.noreply.github.com>
2019-01-15 21:09:19 +0000

    first commit

---
+++ js/file.js
@@ -0,0 +1,10 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("You are " + age + " years old");
+
+
+var days = age * 365;
+alert(age + " years is roughly " + days + " days");
